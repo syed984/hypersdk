ed3795c30530b8d163abc8bdb720a842fdd46d6c
syed984 <152027043+syed984@users.noreply.github.com>
2024-03-02 22:36:17 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "hyperchain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Syed Owaiz"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SO"
 )
 
 var ID ids.ID
